fbc7b12f0d013ba5c4b937f97d5da6bb8965ef4d
KoCus84 <ko.cusveller@gmail.com>
2021-07-08 22:33:11 +0200

    styling and fetching

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,14 +1,34 @@
+//IMPORT LIBRARIES & FUNCTIONS
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Title() {
+//IMPORT STYLING
+import "./Style/Title.css";
+import { motion } from "framer-motion";
+
+export default function Title(projectName) {
 	return (
-		<div>
-			<h1 style={{ fontWeight: 300, marginTop: "20px" }}>
-				Sand{" "}
-				<em>
-					<strong style={{ fontWeight: 1000 }}>BOARD</strong>
-				</em>
-			</h1>
+		<div className="wrap">
+			<motion.div
+				className="portal"
+				whileHover={{
+					x: "5%",
+					y: 0,
+					scale: 1.1,
+					rotate: 0,
+					transition: { duration: 0.5 },
+				}}
+			>
+				<Link to="/" className="portalLink">
+					<h1>portal</h1>
+				</Link>
+			</motion.div>
+
+			<div className="title">
+				<h1 className="projectTitle">{projectName.projectName} </h1>
+				<h1 className="board">BOARD</h1>
+			</div>
+			<div className="emptyRight"></div>
 		</div>
 	);
 }
--- src/Pages/Portal.js
+++ src/Pages/Portal.js
@@ -1,19 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+// IMPORT LIBRARIES
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+// IMPORT CONFIG
+import { apiUrl } from "../Config/constants";
+
+// IMPORT STYLING
 import "./Style/Portal.css";
 
+// IMPORT COMPONENTS
+import Project from "../Components/Project";
+
 export default function Portal() {
+	const [projects, set_projects] = useState([]);
+
+	useEffect(() => {
+		const getProjects = async () => {
+			const res = await axios.get(`${apiUrl}`);
+			set_projects(res.data.projects);
+		};
+		getProjects();
+	}, []);
+
 	return (
-		<div>
-			<div>
-				<h1>portal</h1>
+		<>
+			<div style={{ display: "flex", justifyContent: "space-between" }}>
+				<h1
+					style={{
+						marginLeft: "1vw",
+						marginTop: "1vh",
+					}}
+				>
+					portal
+				</h1>
+				<h1
+					style={{
+						marginRight: "1vw",
+						marginTop: "1vh",
+					}}
+				>
+					login
+				</h1>
 			</div>
-			<div className="title">
-				<Link to="/board">
-					<h1>Sand BOARD</h1>
-				</Link>
+			<div style={{ height: "80vh" }}>
+				{projects.map((projects) => (
+					<Project projects={projects} />
+				))}
 			</div>
-		</div>
+		</>
 	);
 }
