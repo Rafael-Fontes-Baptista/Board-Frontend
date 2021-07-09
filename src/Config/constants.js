02bb12f9829d0e47efcfb2fb17982ffadce98c97
KoCus84 <ko.cusveller@gmail.com>
2021-07-09 13:59:59 +0200

    deploy bugfix

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,5 +1,3 @@
 export const apiUrl =
-	process.env.API_URL ||
-	"http://localhost:4000" ||
-	"https://beachboard.herokuapp.com/";
+	process.env.API_URL || "https://beachboard.herokuapp.com/";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
