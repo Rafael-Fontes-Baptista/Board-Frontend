3816b23e25e557512361c91a864e84a37fc06496
KoCus84 <ko.cusveller@gmail.com>
2021-07-09 08:55:14 +0200

    deploy prep

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,1 +1,4 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+	process.env.API_URL ||
+	"http://localhost:4000" ||
+	"https://beachboard.herokuapp.com/";
